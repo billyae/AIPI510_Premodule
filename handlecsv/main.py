84d018b944a620120241c29cca08df14342a8d31
billyae <euladdggccyy@gmail.com>
2024-08-26 20:43:39 -0400

    update README.md to update the virtual environment name to avoid conlict with the code folder name.
    delete the __pycache__ folder.
    
    Add the required code files for running the project which was deleted accidentally in the previous commit bacause of the same name of virtual environment and the code folder.

--- handlecsv/main.py
+++ handlecsv/main.py
@@ -4,10 +4,10 @@ from .handle_csv import delete_row,delete_column,count_row,count_column
 
 @click.command()
 @click.argument('filename',type=click.Path(exists=True))
-@click.option('--rowindex',default=-1,help='This is the index of row to be deleted in the csvfile')
-@click.option('--columnname',default="-1",help='This is the name of column to be deleted in the csvfile')
-@click.option('--countrow',default=False,help='This is whether you want to count the number of rows in the csvfile after delete')
-@click.option('--countcolumn',default=False,help='This is whether you want to count the number of columns in the csvfile after delete')
+@click.option('--rowindex',type=int,help='This is the index of row to be deleted in the csvfile')
+@click.option('--columnname',type=str,help='This is the name of column to be deleted in the csvfile')
+@click.option('--countrow',type=bool,help='This is whether you want to count the number of rows in the csvfile after delete')
+@click.option('--countcolumn',type=bool,help='This is whether you want to count the number of columns in the csvfile after delete')
 def main(filename,rowindex,columnname,countrow,countcolumn):
     """ Simple programe of doing some basic operations with a specific csv file and modify the csv file based on those operations"""
 
@@ -34,6 +34,3 @@ def main(filename,rowindex,columnname,countrow,countcolumn):
         df.to_csv(f"./output/modified_{filename}",index=False)
 
     print(f"we have successfullly modify this csv file. Here is the modified file ./output/modified_{filename}")
-
-
-
